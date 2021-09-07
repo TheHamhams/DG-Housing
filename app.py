eecc5822280e224d3c2594009800ea57b4657cf5
Corey Hamren <hamhams86@gmail.com>
2021-09-06 19:54:48 -0700

    updated app, index

--- app.py
+++ app.py
@@ -3,21 +3,22 @@ import numpy as np
 import pickle
 
 app = Flask(__name__)
-model=pickle.load('Model/model.pickle', 'rb')
+model=pickle.load(open('Model/model.pickle', 'rb'))
 
 @app.route('/')
 def home():
-    return "Insert home here"
+    return render_template('index.html')
 
-@app.route('/predict', methods='POST')
+@app.route('/predict', methods=['POST'])
 def predict():
-    int_features = [int(x) for x in request.form.values()]
+    
+    int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     pred = model.predict(final_features)
 
     output = round(pred[0], 2)
 
-    return render_template('index.html', prediction_text =f'Housing price prediction: ${output}')
+    return render_template('index.html', prediction_text ='Housing price prediction: ${}'.format(output))
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
